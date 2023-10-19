15b6d06b8c1aa68d22843d754fe27992f6dc32b9
Fionn <fionn010@gmail.com>
2023-10-19 12:26:20 +0100

    feat: initialisation

--- src/main/java/tcd/ie/Main.java
+++ src/main/java/tcd/ie/Main.java
@@ -16,7 +16,7 @@ import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.FSDirectory; 
 
 public class Main {
     private static String INDEX_DIRECTORY = "index";
